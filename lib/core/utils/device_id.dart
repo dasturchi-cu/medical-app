c64b1de88aa60b1f4ea2a556875dce8fb742db73
dasturchi-cu <dasturchi742@gmail.com>
2026-07-29 22:14:48 +0500

    fix: safeguard DeviceId.getStableDeviceId against platform exception

--- lib/core/utils/device_id.dart
+++ lib/core/utils/device_id.dart
@@ -4,29 +4,19 @@ import 'package:device_info_plus/device_info_plus.dart';
 
 class DeviceId {
   static Future<String> getStableDeviceId() async {
-    final plugin = DeviceInfoPlugin();
-
-    if (Platform.isAndroid) {
-      final info = await plugin.androidInfo;
-      return info.id; // build id (stable enough for mock)
-    }
-    if (Platform.isIOS) {
-      final info = await plugin.iosInfo;
-      return info.identifierForVendor ?? 'ios_unknown';
-    }
-    if (Platform.isWindows) {
-      final info = await plugin.windowsInfo;
-      return info.deviceId;
-    }
-    if (Platform.isMacOS) {
-      final info = await plugin.macOsInfo;
-      return info.systemGUID ?? 'mac_unknown';
-    }
-    if (Platform.isLinux) {
-      final info = await plugin.linuxInfo;
-      return info.machineId ?? 'linux_unknown';
-    }
-    return 'unknown_device';
+    try {
+      final plugin = DeviceInfoPlugin();
+      if (Platform.isAndroid) {
+        final info = await plugin.androidInfo;
+        return info.id.isNotEmpty ? info.id : 'android_device';
+      }
+      if (Platform.isIOS) {
+        final info = await plugin.iosInfo;
+        return info.identifierForVendor ?? 'ios_device';
+      }
+    } catch (_) {}
+    return 'device_default';
   }
 }
+
 
